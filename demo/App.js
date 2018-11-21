80e158bd993b3a0fe3c6b546cf2b7b0e006cbf05
vijayst <vijayst@gmail.com>
2018-11-22 00:18:09 +0530

    update build

--- demo/App.js
+++ demo/App.js
@@ -4,7 +4,6 @@ import { Store } from 'react-reducer-store';
 import todoReducer from './todoReducer';
 import Container from './Container';
 
-
 export default function App() {
     return (
         <Store rootReducer={todoReducer}>
